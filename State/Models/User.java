3032e7b647efab0255330642d101a65b281f73e4
Francesco Nieri <francesco.nieri@student.uclouvain.be>
2023-11-06 22:12:05 +0100

    Fixed bug

--- State/Models/User.java
+++ State/Models/User.java
@@ -3,22 +3,29 @@ package State.Models;
 
 import State.Features.Feature;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import State.Features.FeatureName;
+import State.Features.Research;
+import State.Features.Status;
 import State.State;
 
 public class User {
 
     private String name;
     private String password;
-    private Map<FeatureName, Feature> features;
+    private Map<FeatureName, Feature> features = new LinkedHashMap<>();
 
     public User(String name, String password) {
         features = State.getInstance().getFeatures();
         this.name = name;
         this.password = password;
+
+        features.put(FeatureName.Status, new Status());
+        features.put(FeatureName.Research, new Research());
     }
 
     public String getName() {
